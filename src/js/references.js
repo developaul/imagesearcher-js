1d5bd14e39e3755ef29371033825166937f0f405
Paul Chávez <paulchavezromero20@gmail.com>
2020-10-25 16:38:54 -0500

    references added, global references

---
+++ src/js/references.js
@@ -0,0 +1,4 @@
+// References
+export const form       = document.querySelector( '#formulario' ),
+             result     = document.querySelector( '#resultado' ),
+             pagination = document.querySelector( '#paginacion' );
