442323f18797e264359988f00b9ca4a762ffb662
Paul Chávez <paulchavezromero20@gmail.com>
2020-10-25 17:30:43 -0500

    showImages added

--- src/js/classes/ui.class.js
+++ src/js/classes/ui.class.js
@@ -48,6 +48,34 @@ class UI {
         }
     }
 
+    showImages( images ) {
+        this.cleanResult();
+
+        images.forEach( image => {
+            const { previewURL, likes, views, largeImageURL } = image;
+
+            result.innerHTML += `
+                <div class="w-1/2 md:w-1/3 lg:w-1/4 p-3 mb-4">
+                    <div class="bg-white">
+                        <img class="w-full" src="${ previewURL }">
+    
+                        <div class="p-4">
+                            <p class="font-bold"> ${ likes } <span class="font-light"> Me Gusta </span> </p>
+                            <p class="font-bold"> ${ views } <span class="font-light"> Veces Vista </span> </p>
+    
+                            <a  class="block w-full bg-blue-800 hover:bg-blue-500 text-white uppercase font-bold text-center rounder mt-5 p-1"
+                                href="${ largeImageURL }" target="_blank" rel="noopener noreferrer" >
+                                Ver Imagen
+                            </a>
+                        </div>
+                    </div>
+                </div>`;
+        });
+
+        
+
+
+    }
 }
 
 export default UI;
