8d1ddfef735f5b48ce3c0f83b3317f0557f07742
Paul Chávez <paulchavezromero20@gmail.com>
2020-10-25 17:18:37 -0500

    showSpinner added

--- src/js/classes/ui.class.js
+++ src/js/classes/ui.class.js
@@ -1,4 +1,4 @@
-import { form } from '../references.js';
+import { form, result } from '../references.js';
 
 class UI {
     showError( message ) {
@@ -19,6 +19,35 @@ class UI {
             }, 3000 );
         }
     }
+
+    showSpinner() {
+        this.cleanResult();
+
+        const spinnerDiv = document.createElement( 'div' );
+        spinnerDiv.classList.add( 'sk-circle' );
+        spinnerDiv.innerHTML = `
+            <div class="sk-circle1 sk-child"></div>
+            <div class="sk-circle2 sk-child"></div>
+            <div class="sk-circle3 sk-child"></div>
+            <div class="sk-circle4 sk-child"></div>
+            <div class="sk-circle5 sk-child"></div>
+            <div class="sk-circle6 sk-child"></div>
+            <div class="sk-circle7 sk-child"></div>
+            <div class="sk-circle8 sk-child"></div>
+            <div class="sk-circle9 sk-child"></div>
+            <div class="sk-circle10 sk-child"></div>
+            <div class="sk-circle11 sk-child"></div>
+            <div class="sk-circle12 sk-child"></div>`;
+        
+        result.appendChild( spinnerDiv );
+    }
+
+    cleanResult() {
+        while( result.firstChild ) {
+            result.removeChild( result.firstChild );
+        }
+    }
+
 }
 
 export default UI;
